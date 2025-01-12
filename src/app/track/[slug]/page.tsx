77e829788da5880cee4d2776b3ecbf761cd957ec
Tay Nguyen <pttnguyen528@gmail.com>
2025-01-12 21:57:25 +0700

    push code audio

--- src/app/track/[slug]/page.tsx
+++ src/app/track/[slug]/page.tsx
@@ -1,20 +1,20 @@
 "use client";
-import WaveTrack from "@/components/track/wawe.track";
+import WaveTrack from "@/components/track/wave.track";
+import { Container } from "@mui/material";
 import { useSearchParams } from "next/navigation";
 
 const DetailTrackPage = (props: any) => {
-    const { params } = props;
-    const searchParams = useSearchParams();
-    const search = searchParams.get("audio");
-
-    return (
-        <div>
-            DetailTrackPage
-            <div>
-                <WaveTrack />
-            </div>
-        </div>
-    );
+  const { params } = props;
+  const searchParams = useSearchParams();
+  const search = searchParams.get("audio");
+  console.log("search", search);
+  return (
+    <div>
+      <Container>
+        <WaveTrack />
+      </Container>
+    </div>
+  );
 };
 
 export default DetailTrackPage;
--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -1,25 +1,51 @@
 import { useState, useEffect } from "react";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
 export const useHasMounted = () => {
-    const [hasMounted, setHasMounted] = useState<boolean>(false);
-    useEffect(() => {
-        setHasMounted(true);
-    }, []);
+  const [hasMounted, setHasMounted] = useState<boolean>(false);
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
 
-    return hasMounted;
+  return hasMounted;
 };
 
 export const useScript = (url: string) => {
-    useEffect(() => {
-        const script = document.createElement("script");
+  useEffect(() => {
+    const script = document.createElement("script");
 
-        script.src = url;
-        script.async = true;
+    script.src = url;
+    script.async = true;
 
-        document.body.appendChild(script);
+    document.body.appendChild(script);
 
-        return () => {
-            document.body.removeChild(script);
-        };
-    }, [url]);
+    return () => {
+      document.body.removeChild(script);
+    };
+  }, [url]);
+};
+
+// WaveSurfer hook
+export const useWavesurfer = (
+  containerRef: React.RefObject<HTMLDivElement>,
+  options: Omit<WaveSurferOptions, "container">
+) => {
+  const [wavesurfer, setWavesurfer] = useState<WaveSurfer | null>(null);
+
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const ws = WaveSurfer.create({
+      ...options,
+      container: containerRef.current,
+    });
+
+    setWavesurfer(ws);
+
+    return () => {
+      ws.destroy();
+    };
+  }, [options, containerRef]);
+
+  return wavesurfer;
 };
